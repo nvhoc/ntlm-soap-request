39a61a368412ebe4a5f8f3bfb44f6ac2e3812096
Hoc Nguyen <hoc.nguyenviet90@gmail.com>
2016-01-13 15:58:10 +0700

    v0.1

---
+++ index.js
@@ -0,0 +1,67 @@
+/**
+ * Created by hocnguyen on 1/13/16.
+ */
+var ntlm = require('httpntlm').ntlm;
+var async = require('async');
+var httpreq = require('httpreq');
+var HttpsAgent = require('agentkeepalive').HttpsAgent;
+var keepaliveAgent = new HttpsAgent();
+var fs = require('fs');
+
+var options = {
+    url: "https://someurl.com",
+    username: 'nav1-advania@skaginn.local',
+    password: 'Skaginn.300',
+    workstation: 'choose.something',
+    domain: ''
+};
+
+module.exports = {
+    'request': function(opt,cb) {
+        var fileData = fs.readFileSync(opt.filePath, 'utf8');
+        options.url = opt.url;
+        async.waterfall([
+            function (callback) {
+                var type1msg = ntlm.createType1Message(options);
+
+                httpreq.post(options.url, {
+                    headers: {
+                        'Connection': 'keep-alive',
+                        'Authorization': type1msg,
+                        'Content-Type': 'text/xml;charset=UTF-8',
+                        'SOAPAction': opt.soapAction
+                    },
+                    body: fileData,
+                    agent: keepaliveAgent
+                }, callback);
+            },
+
+            function (res, callback) {
+                if (!res.headers['www-authenticate'])
+                    return callback(new Error('www-authenticate not found on response of second request'));
+
+                var type2msg = ntlm.parseType2Message(res.headers['www-authenticate']);
+                var type3msg = ntlm.createType3Message(type2msg, options);
+
+                httpreq.post(options.url, {
+                    headers: {
+                        'Connection': 'Close',
+                        'Authorization': type3msg,
+                        'Content-Type': 'text/xml;charset=UTF-8',
+                        'SOAPAction': opt.soapAction
+                    },
+                    'body':fileData,
+                    allowRedirects: false,
+                    agent: keepaliveAgent
+                }, callback);
+            }
+        ], function (err, res) {
+            if (err) return console.log(err);
+
+            console.log(res.headers);
+            console.log(res.body);
+            cb(err, res);
+        });
+    }
+
+};
